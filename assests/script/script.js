55073a19d7d6016517b80a6650cd5b93287480df
Karem <karemdarwesh1035@gmail.com>
2023-12-08 16:37:02 +0200

    karem's last changes

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,24 +1,35 @@
-// Use addEventListener to attach the scroll event handler
 window.addEventListener('scroll', scrollFunction);
 
 function scrollFunction() {
-  // Use document.documentElement.scrollTop || document.body.scrollTop for cross-browser compatibility
   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-
-  // Get the 'navbar' element
   var navbar = document.getElementById('navbar');
   var logo = document.getElementById('logo');
+  var topbar = document.getElementById('topbar');
 
-  // Adjust the padding based on the scroll position
   if (scrollTop >= 20) {
-    navbar.style.padding = '20px 0';
-    logo.style.width = '60%'; // Change the padding values to your desired values
+    navbar.style.padding = '20px';
+    logo.style.width = '60%';
+    topbar.style.display = 'none';
   } else {
-    navbar.style.padding = '30px 0'; // Change the padding values to your desired values
+    navbar.style.padding = '30px';
     logo.style.width = '80%';
+    topbar.style.display = 'block'; // Adjust to show the top bar when scrolling up
   }
 }
 
-// Add CSS transition to the 'navbar' element
+// Add CSS transition to the 'navbar' and 'logo' elements
 document.getElementById('navbar').style.transition = 'padding 0.5s ease';
 document.getElementById('logo').style.transition = 'width 0.5s ease';
+
+document.querySelectorAll('.navbar-nav a').forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    document.querySelector('.navbar-toggler').click(); // Simulate a click on the toggle button
+  });
+});
+
+// Close the navbar when the user scrolls
+window.addEventListener('scroll', () => {
+  if (document.body.classList.contains('show')) {
+    document.querySelector('.navbar-toggler').click(); // Simulate a click on the toggle button
+  }
+});
